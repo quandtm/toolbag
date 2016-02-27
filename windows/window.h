2989745886cd67a3ba2c6fa3e949c0ee0b382e07
Michael Quandt <michael@mquandt.com>
2016-02-28 01:38:36 +1100

    Add window class

---
+++ windows/window.h
@@ -0,0 +1,122 @@
+#pragma once
+#ifdef _WINDOWS
+#define WIN32_LEAN_AND_MEAN
+#include <Windows.h>
+#include <stdint.h>
+
+struct Window
+{
+	HWND hwnd = 0;
+	bool exists = false;
+	bool quitOnDestroy = false;
+
+	Window() = default;
+	Window(const Window&) = delete;
+	~Window();
+
+	const bool Create(const char *windowTitle, const uint32_t width, const uint32_t height);
+	const bool NextMessage(MSG *msg, const bool thisWindowOnly = false);
+	void Destroy();
+
+	static void UnusedMessage(const MSG *msg);
+};
+
+#ifdef WINDOW_IMPL
+EXTERN_C IMAGE_DOS_HEADER __ImageBase;
+#define HINST_THISCOMPONENT ((HINSTANCE)&__ImageBase)
+
+__forceinline HWND ParamsToHwnd(WPARAM wParam, LPARAM lParam)
+{
+#ifdef _M_X64
+		return (HWND)(((uint64_t)wParam << 32) | (uint64_t)lParam);
+#else
+		return (HWND)(((uint32_t)wParam << 16) | (uint32_t)lParam);
+#endif
+}
+
+__forceinline void HwndToParams(HWND hwnd, WPARAM *wParam, LPARAM *lParam)
+{
+#ifdef _M_X64
+		*wParam = (UINT)(((uint64_t)hwnd & 0xFFFFFFFF00000000) >> 32);
+		*lParam = (UINT)((uint64_t)hwnd & 0x00000000FFFFFFFF);
+#else
+		*wParam = (UINT)((uint32_t)hwnd & 0xFFFF0000) >> 16;
+		*lParam = (UINT)((uint32_t)hwnd & 0x0000FFFF);
+#endif
+}
+
+LRESULT WINAPI WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+	switch (msg)
+	{
+		case WM_DESTROY:
+			LPARAM l;
+			WPARAM w;
+			HwndToParams(hwnd, &w, &l);
+			PostMessageA(NULL, WM_DESTROY, w, l);
+			return 0;
+
+		default:
+			return DefWindowProcA(hwnd, msg, wParam, lParam);
+	}
+}
+
+const bool Window::Create(const char *windowTitle, const uint32_t width, const uint32_t height)
+{
+	HINSTANCE hinst = HINST_THISCOMPONENT;
+	WNDCLASSA cl = {};
+	cl.lpfnWndProc = WndProc;
+	cl.hInstance = hinst;
+	cl.lpszClassName = "window";
+	ATOM c = RegisterClassA(&cl);
+	if (c == 0)
+		return false;
+	hwnd = CreateWindowA("window", windowTitle, WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, width, height, NULL, NULL, hinst, NULL);
+	if (hwnd == NULL)
+		return false;
+	ShowWindow(hwnd, SW_SHOW);
+	exists = true;
+	return true;
+}
+
+const bool Window::NextMessage(MSG *msg, const bool thisWindowOnly)
+{
+	BOOL ret = PeekMessageA(msg, (thisWindowOnly ? hwnd : NULL), 0, 0, PM_REMOVE);
+	if (msg->message == WM_DESTROY && hwnd == ParamsToHwnd(msg->wParam, msg->lParam))
+	{
+		exists = false;
+		if (quitOnDestroy)
+			PostQuitMessage(0);
+		return false;
+	}
+	return ret > 0;
+}
+
+void Window::UnusedMessage(const MSG *msg)
+{
+	TranslateMessage(msg);
+	DispatchMessageA(msg);
+}
+
+Window::~Window()
+{
+	Destroy();
+}
+
+void Window::Destroy()
+{
+	if (hwnd != 0)
+		DestroyWindow(hwnd);
+	exists = false;
+}
+#endif
+
+#ifdef WINMAIN_WRAPPER
+#include <stdlib.h>
+int _cdecl main(int argc, char **argv);
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+{
+	return main(__argc, __argv);
+}
+#endif
+#endif
